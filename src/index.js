093374e268bd7c26d12e3ae4dd7b44ae4cc61c45
Valentine Education Center <val.edu.center@gmail.com>
2022-05-15 13:09:32 -0500

    pray

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css"
 import App from "./App"
 import './index.css'
 import configureStore from './redux/configureStore'
 import { Provider as ReduxProvider } from 'react-redux'
+import { createRoot } from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
 
 const loadFromLocalStorage = () => {
     try {
@@ -30,4 +31,13 @@ const store = configureStore(loadFromLocalStorage())
 store.subscribe(() => {
     saveToLocalStorage(store.getState());
 });
-render(<ReduxProvider store={store}><Router><App /></Router></ReduxProvider>, document.getElementById('app'))
+
+const container = document.getElementById('app')
+console.log(container)
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+root.render(<ReduxProvider store={store}><Router><App /></Router></ReduxProvider>)
+
+ // If you want to start measuring performance in your app, pass a function
+ // to log results (for example: reportWebVitals(console.log))
+ // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ reportWebVitals();
